058ac7fe6aa64a57f827eb20613053622af9fc63
TanoueKazuya <98604082+TanoueKazuyaGGJ@users.noreply.github.com>
2022-01-29 18:14:38 +0900

    ゲームスタート画面の追加

--- Assets/SceneChangeOperation.cs
+++ Assets/SceneChangeOperation.cs
@@ -2,6 +2,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneChangeOperation
 {
+    /// <summary>
+    /// シーンチェンジ関数
+    /// </summary>
+    /// <param name="SceneName">シーン名</param>
     void SceneChange (string SceneName)
     {
         SceneManager.LoadScene(SceneName);
