3e0bc3cc6decbcb0ab9a0181717dbeac1f469495
TanoueKazuya <98604082+TanoueKazuyaGGJ@users.noreply.github.com>
2022-01-30 12:06:08 +0900

    ゲームクリアの基準の設定

--- Assets/Scripts/GameFlowOperation.cs
+++ Assets/Scripts/GameFlowOperation.cs
@@ -6,6 +6,8 @@ public class GameFlowOperation
 {
     //繁栄評価値　0.0fから100.0fまで
     public float _gamePoint = 0.0f;
+    public float _gameClearPoint = 10000.0f;
+
     public enum _creaturePointType
     {
         Plancton,
@@ -20,6 +22,7 @@ public class GameFlowOperation
         Gorilla
     }
 
+    //生物をカウントして加算させる
     public void CreaturePoint(List<Creature> creatureList)
     {
 
