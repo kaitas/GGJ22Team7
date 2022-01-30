5b1abb10439a827d28d0e1df8d677ae97e4c801c
Taki-ayst <98199786+Taki-ayst@users.noreply.github.com>
2022-01-30 10:26:01 +0900

    マップデータロードできる　今からセルを埋めるのを作る
    からの風呂輪を作る
    ｘを90度

---
+++ Assets/Scripts/TestMap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMap : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = -50; x <= 50; x++)
+        {
+            for (int y = -50; y <= 50;y++)
+            {
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
