98b80070ed8d33f711a86d2d7b988edc17012603
TanoueKazuya <98604082+TanoueKazuyaGGJ@users.noreply.github.com>
2022-01-30 13:50:15 +0900

    CreateCellにCellを生成した際に、Listに入れる処理の追加

--- Assets/Scripts/CreateCell.cs
+++ Assets/Scripts/CreateCell.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class CreateCell : MonoBehaviour
 {
     public GameObject Cell;
-
+    public List<GameObject[]> _cell_list;
     public int _cell_y;@//‚Æ‚è‚ ‚¦‚¸‚‚³‚ğ’²®‚Å‚«‚é
 
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(Cell, new Vector3 (0,0,0) , Quaternion.Euler(90,0,0));
-
+        _cell_list = new List<GameObject[]>();
         for (int x = -50; x <= 50; x++)
         {
+            int Yaxis_count = 0;
+            GameObject[] cell_Yaxis_objs = new GameObject[100];
             for (int y = -50; y <= 50; y++)
             {
-                Instantiate(Cell, new Vector3(x, _cell_y, y), Quaternion.Euler(90, 0, 0));
-            }
-        }
+                GameObject clone = Instantiate(Cell, new Vector3(x, _cell_y, y), Quaternion.Euler(90, 0, 0));
+                cell_Yaxis_objs[Yaxis_count] = clone;
 
-        //GameObject.FindWithTag("floor");
+            }
+            _cell_list.Add(cell_Yaxis_objs);
+        }
 
     }
 
